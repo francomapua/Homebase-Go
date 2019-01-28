4e7d3e0fc39005c52cd6ceb11cff4a59946a8bb6
Franco Luis Mapua <franco@migo.tv>
2019-01-28 15:52:56 +0800

    Rotating Logs
    Improving to version 2, now with log count

--- rotatinglogs/RotateWriter.go
+++ rotatinglogs/RotateWriter.go
@@ -1,39 +1,56 @@
-package rotatinglogs
+// Version 2
+package logger
 
 import (
 	"os"
+	"path/filepath"
+	"sort"
+	"strconv"
+	"strings"
 	"sync"
 	"time"
 )
 
-// RotateWriter : Struct, please don't use this
+const (
+	maxLogFiles int = 5
+)
+
+// RotateWriter : Struct
 type RotateWriter struct {
 	lock     sync.Mutex
 	filename string
 	fp       *os.File
 }
 
-// New : Constructor
-func New(filename string) *RotateWriter {
+// NewRotateWriter : Constructor
+func NewRotateWriter(filename string) *RotateWriter {
+	filename, _ = filepath.Abs(filename)
 	writer := &RotateWriter{filename: filename}
-	err := writer.Rotate()
-	if err != nil {
-		return nil
-	}
 	return writer
 }
 
-// Write : writes a log to a file
+// Write : Basic logging function
 func (writer *RotateWriter) Write(output []byte) (bytesWritten int, err error) {
+	// Check for Rotation
+	if stat, err := os.Stat(writer.filename); os.IsNotExist(err) || !writer.areDatesOnSameDay(stat.ModTime(), time.Now()) {
+		// If log file does not exist OR log file was last editted before today
+		writer.rotate()
+	}
+
+	// Mutex Lock
 	writer.lock.Lock()
 	defer writer.lock.Unlock()
+
+	// Write to file
+	writer.fp, err = os.OpenFile(writer.filename, os.O_WRONLY|os.O_APPEND, 0644)
+	defer writer.fp.Close()
 	bytesWritten, err = writer.fp.Write(output)
+
 	return bytesWritten, err
 }
 
-// Rotate : archives and deletes files after a certain number of rotations
-func (writer *RotateWriter) Rotate() (err error) {
-
+func (writer *RotateWriter) rotate() (err error) {
+	// Mutex Lock
 	writer.lock.Lock()
 	defer writer.lock.Unlock()
 
@@ -46,16 +63,62 @@ func (writer *RotateWriter) Rotate() (err error) {
 		}
 	}
 
-	// Rename Dest File if it Exists
-	if _, err = os.Stat(writer.filename); !os.IsNotExist(err) {
-		err = os.Rename(writer.filename, writer.filename+"."+time.Now().Format(time.RFC3339))
+	// Rename Old File If it Exists
+	if stat, err := os.Stat(writer.filename); !os.IsNotExist(err) {
+		err = os.Rename(writer.filename, writer.filename+"-"+writer.getSimpleDate(stat.ModTime()))
 		if err != nil {
 			return err
 		}
 	}
 
-	// Delete the old files
 	// Create File
+	writer.upsertDirectory(filepath.Dir(writer.filename))
 	writer.fp, err = os.Create(writer.filename)
+
+	// Trim files if there are too many logs
+	var logFiles []os.FileInfo
+	dir, _ := filepath.Abs(filepath.Dir(writer.filename))
+	logname := filepath.Base(writer.filename)
+
+	filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		name := info.Name()
+		print(name)
+		if strings.Contains(info.Name(), logname) {
+			logFiles = append(logFiles, info)
+		}
+		return nil
+	})
+	if len(logFiles) > maxLogFiles {
+		// Sort Slice by edit date
+		sort.Slice(logFiles, func(a, b int) bool {
+			return logFiles[a].ModTime().After(logFiles[b].ModTime())
+		})
+		// Delete Other Logs
+		for i := len(logFiles); i > maxLogFiles; i-- {
+			deletePath, _ := filepath.Abs(dir + "/" + logFiles[i-1].Name())
+			os.Remove(deletePath)
+		}
+	}
+	return nil
+}
+
+/* Helper Functions */
+func (writer *RotateWriter) areDatesOnSameDay(dateA time.Time, dateB time.Time) bool {
+	yearA, monthA, dayA := dateA.Date()
+	yearB, monthB, dayB := dateB.Date()
+	return yearA == yearB && monthA == monthB && dayA == dayB
+}
+func (writer *RotateWriter) getSimpleDate(dateTime time.Time) string {
+	year, month, day := dateTime.Date()
+	runes := []rune(strings.ToUpper(month.String()))
+	monthShort := string(runes[0:3])
+	concatString := strconv.Itoa(year) + monthShort + strconv.Itoa(day)
+	return concatString
+}
+func (writer *RotateWriter) upsertDirectory(path string) error {
+	var err error
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		err = os.MkdirAll(path, 0755)
+	}
 	return err
 }
